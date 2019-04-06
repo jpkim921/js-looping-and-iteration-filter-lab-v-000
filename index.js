0e66b4e622cff68ae91692e80793250c3f8ff34b
Jayon Kim <jpkim921@gmail.com>
2019-04-06 03:22:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-function findMatching (drivers, name){
-  return drivers.filter(function(driver) {
-      return driver === name
-  })
-}
+// function findMatching (drivers, name){
+//   return drivers.filter(function(driver) {
+//       return driver === name
+//   })
+// }
 
 
 function findMatching (drivers, name){
